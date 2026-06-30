b7b1e622f6ec6a80925d97666527b6875c0cb890
Luke Aguilar <dev@rdxip.com>
2026-06-30 13:38:15 +1000

    feat: Add connector identification image

--- demo/SD.cpp
+++ demo/SD.cpp
@@ -11,3 +11,4 @@ void loop()
 
 // TODO Preallocate sd card space for race mode
 // TODO Do we need to flush sd to not corrupt in power down failure? How does this work?
+// TODO Should we also log which driver is currently driving along with the data?
