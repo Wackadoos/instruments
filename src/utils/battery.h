38392872823368f08f328202d79c2bb4992a0934
Luke Aguilar <dev@rdxip.com>
2026-07-06 17:03:22 +1000

    chore: Add notes of todos

---
+++ src/utils/battery.h
@@ -0,0 +1,3 @@
+#pragma once
+
+// TODO Estimate using correction function (Peukert's Law) and temperature for running discharge average rate and Coulomb counting from ESC
---
+++ src/utils/metrics.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// TODO Count clock cycles for each task
+// TODO Keep track of average and longest + average & longest main loop time
+// TODO Measure time from sensor request to sensor data update (in state)
